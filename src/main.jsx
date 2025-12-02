22b6c8027bff7a9595c73a1885be7b91f4ff0e71
WHISPAR <wilsonwasili7@gmail.com>
2025-12-02 23:23:06 +0200

    Add documentation header to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+/**
+ * Application entry point
+ * Renders the root App component with React StrictMode enabled
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
